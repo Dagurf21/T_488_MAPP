6c8d320d18b6b85f1a98590356e94c53387195b0
Dagurf21 <dagur.freyr2820@gmail.com>
2024-11-25 11:58:27 +0000

    Making eslint and prettier work

---
+++ the-toddler/app/screens/boardView.js
@@ -0,0 +1,23 @@
+// screens/BoardView.js
+
+import React from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+
+const BoardView = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Boards</Text>
+      <Button title={'Create New Board'} onPress={() => {}} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default BoardView;
